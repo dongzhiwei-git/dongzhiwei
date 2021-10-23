ff576ca27885d82c9c43440e61720489407522d2
dongzhiwei <2660847388@qq.com>
2021-10-23 11:49:51 +0800

    init

---
+++ internal/router/routher.go
@@ -0,0 +1,33 @@
+package router
+
+import (
+	"fmt"
+	"inherited/internal/api"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() {
+	var r *gin.Engine
+	r = gin.Default()
+
+	// to solve the cross domain
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, nil)
+	})
+
+	var admin = r.Group("/admin")
+	{
+		admin.POST("/reg", api.CreateAdminUser)
+	}
+
+	// setup listen
+	err := r.Run(":8000")
+	if err != nil {
+		fmt.Printf("run failed: %v\n", err)
+		return
+
+	}
+
+}
